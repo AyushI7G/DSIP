e5f2bfb1c6e2539f5bf84b90f69db6ad8734e624
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-02-12 23:57:45 +0530

    Add files via upload

---
+++ bitps.m
@@ -0,0 +1,18 @@
+r1 = input('Enter r1');
+r2 = input('Enter r2');
+
+for i = 1:128
+    for j = 1:128
+        r = A(i,j);
+        if r <= r1 
+            
+        else if r <= r2
+            
+        else
+                
+        end
+        end
+    end
+end
+
+imshow(D)  
